2afdde7948430da1a5baa3c1ed1006b3401cad6e
rabbanictgbd <rabbanictgbd@gmail.com>
2025-06-23 10:32:43 +0600

    fixed null email problem by && oparator

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -6,17 +6,19 @@ import { useParams } from 'react-router';
 const CartPage = () => {
     const { serverApi } = useContext(AuthContext)
     const {email}= useParams()
-    const [carts, setCarts] = useState([null])
+    const [carts, setCarts] = useState([])
     fetch(`${serverApi}/carts/${email}`)
         .then(res => res.json())
         .then(data => {
             console.log(data)
-            setCarts
+            setCarts(data)
         })
     return (
         <div>
             <h1>
                 {carts.length}
+                <br />
+                {email.length}
             </h1>
         </div>
     );
